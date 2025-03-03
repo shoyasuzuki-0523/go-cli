19bb8c39c272e589d872e2b026f1a5dd3a86ee35
shoyasuzuki-0523 <bellwood0523@gmail.com>
2025-03-03 11:46:14 +0900

    fix

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module go-cli
 
 go 1.24.0
+
+require github.com/samber/lo v1.49.1
+
+require golang.org/x/text v0.21.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/samber/lo"
 )
 
 func main() {
 	arg1 := os.Args[1]
-	fmt.Printf("Hello, %s!\n", arg1)
+	sample := lo.Map([]string{"hoge", "fuga"}, func(s string, i int) string {
+		return fmt.Sprintf("%d: %s", i, s)
+	})
+	fmt.Printf("Hello, %s!, %s\n", arg1, strings.Join(sample, ", "))
 }
