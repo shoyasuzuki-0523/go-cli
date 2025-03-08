3bc89a15b3d4d8ab9499305b9f4ddadba5c05c8c
shoyasuzuki-0523 <bellwood0523@gmail.com>
2025-03-09 00:56:09 +0900

    fix

--- main.go
+++ main.go
@@ -2,16 +2,55 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
-
-	"github.com/samber/lo"
+	"math"
 )
 
+type combo struct {
+	x, y, z int
+}
+
+func fermat(w, n int) []*combo {
+	var ok bool
+	var result []*combo
+
+	for x := 1; x <= w; x++ {
+		for y := 1; y <= w; y++ {
+			for z := 1; float64(z) <= math.Pow(float64(x), float64(n))+math.Pow(float64(y), float64(n)); z++ {
+				ok = math.Pow(float64(x), float64(n))+math.Pow(float64(y), float64(n)) == math.Pow(float64(z), float64(n))
+				if ok {
+					result = append(result, &combo{x, y, z})
+				}
+			}
+		}
+	}
+
+	return result
+}
+
 func main() {
-	arg1 := os.Args[1]
-	sample := lo.Map([]string{"hoge", "fuga"}, func(s string, i int) string {
-		return fmt.Sprintf("%d: %s", i, s)
-	})
-	fmt.Printf("Hello, %s!, %s\n", arg1, strings.Join(sample, ", "))
+	fmt.Printf("input: [%d %d]\n", 10, 2)
+	for _, v := range fermat(10, 2) {
+		fmt.Printf("%d %d %d\n", v.x, v.y, v.z)
+	}
+
+	fmt.Println("----")
+
+	fmt.Printf("input: [%d %d]\n", 20, 2)
+	for _, v := range fermat(20, 2) {
+		fmt.Printf("%d %d %d\n", v.x, v.y, v.z)
+	}
+
+	fmt.Println("----")
+
+	fmt.Printf("input: [%d %d]\n", 10, 3)
+	for _, v := range fermat(10, 3) {
+		fmt.Printf("%d %d %d\n", v.x, v.y, v.z)
+	}
+
+	fmt.Println("----")
+
+	fmt.Printf("input: [%d %d]\n", 100, 7)
+	for _, v := range fermat(100, 7) {
+		fmt.Printf("%d %d %d\n", v.x, v.y, v.z)
+	}
 }
